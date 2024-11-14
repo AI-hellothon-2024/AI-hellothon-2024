53fabd72eeb3709e08cdbe0ea982263d86c37dad
Hongsc0125 <hsc0125@knou.ac.kr>
2024-11-14 13:44:28 +0900

    프로젝트 기본 구성

--- app/db/session.py
+++ app/db/session.py
@@ -1,11 +1,11 @@
 # DB 연결 설정 및 세션관리
-# from sqlalchemy import create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-# from app.core.config import settings
-#
-# SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-#
-# engine = create_engine(SQLALCHEMY_DATABASE_URL)
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Base = declarative_base()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app.core.config import settings
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()

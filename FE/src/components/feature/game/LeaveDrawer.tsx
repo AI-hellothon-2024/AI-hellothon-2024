4bb11a26ab02ee003352c92073e56b037eaa4908
sasha1107 <sasha1107@naver.com>
2024-11-18 22:52:06 +0900

    feat: 대화종료 스타일 수정

--- FE/src/components/feature/game/LeaveDrawer.tsx
+++ FE/src/components/feature/game/LeaveDrawer.tsx
@@ -10,7 +10,7 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "@/components/ui/button";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, X } from "lucide-react";
 
 const LeaveDrawer = () => {
   return (
@@ -18,9 +18,12 @@ const LeaveDrawer = () => {
       <DrawerTrigger>
         <ChevronLeft className="ml-2" />
       </DrawerTrigger>
-      <DrawerContent className="max-w-[600px] mx-auto">
+      <DrawerContent className="max-w-[600px] mx-auto bg-[#1E1E1E] border-[#1E1E1E]">
         <DrawerHeader>
-          <DrawerTitle className="text-center pt-16">
+          <DrawerClose className="absolute top-4 right-4">
+            <X />
+          </DrawerClose>
+          <DrawerTitle className="text-center pt-16 text-xl">
             대화를 종료하시겠습니까?
           </DrawerTitle>
           <DrawerDescription className="text-center pt-5 pb-11">
@@ -30,11 +33,17 @@ const LeaveDrawer = () => {
           </DrawerDescription>
         </DrawerHeader>
         <DrawerFooter className="gap-2.5">
-          <Button asChild className="rounded-full">
+          <Button
+            asChild
+            className="rounded-full py-3 h-auto text-white font-semibold text-lg"
+          >
             <Link href={"/"}>종료하기</Link>
           </Button>
           <DrawerClose asChild>
-            <Button variant="outline" className="w-full rounded-full">
+            <Button
+              variant="outline"
+              className="w-full rounded-full py-3 h-auto text-white font-semibold text-lg"
+            >
               취소하기
             </Button>
           </DrawerClose>

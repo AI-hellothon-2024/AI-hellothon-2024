3452faa9b9d12d579174640d09616cd4eb65c3c7
sasha1107 <sasha1107@naver.com>
2024-11-22 02:45:13 +0900

    feat: local에서만 devtools 사용

--- FE/src/components/provider/QueryProvider.tsx
+++ FE/src/components/provider/QueryProvider.tsx
@@ -39,7 +39,9 @@ export default function QueryProvider({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {window.location.protocol === "http:" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 }

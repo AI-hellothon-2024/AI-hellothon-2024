97d5ecd9d1b87998bd573fb8eff8bb8d3f5c5aba
sasha1107 <sasha1107@naver.com>
2024-11-24 08:53:56 +0900

    feat: main animation delay

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -32,12 +32,18 @@ export default function Home() {
           <MotionDiv
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.5,
+            }}
           >
             사회초년생을 위한
           </MotionDiv>
           <MotionDiv
             initial={{ opacity: 0, y: -20 }}
             whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.5,
+            }}
           >
             <Image src="/logo.png" width={129} height={40} alt="AImigo" />
           </MotionDiv>

c76f55a5a68ce8cf7f3ec73a8d148966b6ba783a
sasha1107 <sasha1107@naver.com>
2024-11-23 14:56:12 +0900

    feat: 로딩 화면 수정

--- FE/src/utils/getRandomPersonality.ts
+++ FE/src/utils/getRandomPersonality.ts
@@ -2,7 +2,9 @@ import { PERSONALITIES } from "@/lib/constants";
 
 const getRandomPersonality = () => {
   const personalities = Object.keys(PERSONALITIES);
-  return personalities[Math.floor(Math.random() * personalities.length)];
+  return personalities[
+    Math.floor(Math.random() * personalities.length)
+  ] as keyof typeof PERSONALITIES;
 };
 
 export default getRandomPersonality;

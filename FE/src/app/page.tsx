6d98e801bf0d2770f7c48612c05cac4b789dcde8
sasha1107 <sasha1107@naver.com>
2024-11-23 14:31:50 +0900

    feat: 배경 캐러셀 없애기

--- FE/src/app/page.tsx
+++ FE/src/app/page.tsx
@@ -3,43 +3,29 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { MotionDiv } from "@/components/motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
-import { ChevronLeft, ChevronRight } from "lucide-react";
 import GalleryButton from "@/components/feature/main/GalleryButton";
+import { getRandomNumber } from "@/lib/utils";
 
 export default function Home() {
-  const [bg, setBg] = useState(1);
+  const [bg, setBg] = useState(getRandomNumber(1, 26));
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBg(getRandomNumber(1, 26));
+    }, 8000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <MotionDiv
-      className="w-full h-dvh overflow-y-clip bg-center grid grid-rows-5 transition-all bg-cover bg-no-repeat "
+      className="w-full h-dvh overflow-y-clip bg-center grid grid-rows-5 transition-all bg-cover bg-no-repeat"
       style={{
-        backgroundSize: "110%",
         backgroundImage: `url(/splash/${bg}.png)`,
       }}
     >
-      <div className="row-span-2">
-        <div className="self-start mb-4 flex mx-auto backdrop-blur rounded-full border w-fit px-5 py-2 font-bold items-center text-primary gap-2">
-          <Button
-            variant={"secondary"}
-            className="rounded-full aspect-square"
-            onClick={() => {
-              setBg((prev) => (prev === 26 ? 1 : prev + 1));
-            }}
-          >
-            <ChevronLeft />
-          </Button>
-          <Button
-            variant={"secondary"}
-            className="rounded-full aspect-square"
-            onClick={() => {
-              setBg((prev) => (prev === 1 ? 26 : prev - 1));
-            }}
-          >
-            <ChevronRight />
-          </Button>
-        </div>
-      </div>
+      <div className="row-span-2" />
 
       <div className="bg-gradient-to-b to-[#1E1E1E] from-transparent h-full px-9 flex flex-col justify-end pb-[74px] gap-14 row-span-3 via-60% via-[#1E1E1E]">
         <div className="flex flex-col items-center gap-3">

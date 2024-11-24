8702700d2da6959ec9f4d5b61d4a09bb5d4dea5e
sasha1107 <sasha1107@naver.com>
2024-11-24 11:38:58 +0900

    feat: gallery situatuin icon

--- FE/src/app/gallery/Gallery.tsx
+++ FE/src/app/gallery/Gallery.tsx
@@ -11,6 +11,7 @@ import { ChevronLeft } from "lucide-react";
 import { groupBy } from "@/lib/utils";
 import { twJoin, twMerge } from "tailwind-merge";
 import Image from "next/image";
+import SituationIcon from "@/components/common/SituationIcon";
 
 const Gallery = () => {
   const userId = useAtomValue(userIdAtom);
@@ -32,12 +33,18 @@ const Gallery = () => {
           </h1>
         </div>
       </header>
-      <div className="flex flex-col px-4 gap-9">
+      <div className="flex flex-col px-4 gap-9 py-10">
         {data
           ? Object.entries(groupBy(data.result, (data) => data.situation)).map(
               ([situation, results]) => (
                 <div key={situation} className="flex flex-col gap-4">
-                  <h2 className="text-white font-DungGeunMo w">
+                  <h2 className="text-white font-DungGeunMo w flex gap-1 items-center">
+                    <div className="w-[14px] aspect-square">
+                      <SituationIcon
+                        fill="white"
+                        situation={situation as keyof typeof SITUATIONS}
+                      />
+                    </div>
                     {SITUATIONS[situation as keyof typeof SITUATIONS]}
                   </h2>
                   <div className="grid grid-cols-3 gap-1">
